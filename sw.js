c2dc03ab6ecb35ea41dcff199a996df630ad7a48
hammerbamsen <kennet@hammerby.com>
2026-06-07 05:51:35 +0200

    SW: ingen cache overhovedet

--- sw.js
+++ sw.js
@@ -1,29 +1,8 @@
-const CACHE = "faf-v1";
-
-self.addEventListener("install", e => {
-  self.skipWaiting();
-});
-
+// v202606070351
+self.addEventListener("install", e => self.skipWaiting());
 self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener("fetch", e => {
-  // Kun GET requests
-  if (e.request.method !== "GET") return;
-
-  e.respondWith(
-    fetch(e.request, { cache: "no-store" })
-      .then(res => {
-        // Gem kopi i cache som fallback
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request, {cache:"no-store"}));
 });
